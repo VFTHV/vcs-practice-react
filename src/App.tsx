b9b7b55d9e9a17c40fc75438ab722859dcb1d894
Vadim Fattakhov <vadfthv@gmail.com>
2024-06-22 17:50:24 -0500

    MuiSelect

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import './App.css';
+// import './App.css';
 import MuiButton from './components/MuiButton';
+import MuiSelect from './components/MuiSelect';
 import { MuiTypography } from './components/MuiTypography';
 
 function App() {
   return (
-    <div>
+    <div style={{ margin: '2rem' }}>
       {/* <MuiTypography /> */}
-      <MuiButton />
+      {/* <MuiButton /> */}
+      <MuiSelect />
     </div>
   );
 }
