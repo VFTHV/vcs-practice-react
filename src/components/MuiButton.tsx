369c3ad204308fffd195ea67e84c7848e807e151
Vadim Fattakhov <vadfthv@gmail.com>
2024-06-22 15:42:30 -0500

    random commit

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -77,6 +77,7 @@ export default function MuiButton() {
           <Button>Center</Button>
           <Button>Right</Button>
         </ButtonGroup>
+        {/* center */}
       </Stack>
     </Stack>
   );
