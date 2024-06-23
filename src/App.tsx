2ce0d99fd1c3e92a29532d3ec73199bea17a890d
Vadim Fattakhov <vadfthv@gmail.com>
2024-06-23 12:32:42 -0500

    MuiCheckbox.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 // import './App.css';
-import MuiButton from './components/MuiButton';
-import MuiSelect from './components/MuiSelect';
-import { MuiTypography } from './components/MuiTypography';
+// import MuiButton from './components/MuiButton';
+// import MuiSelect from './components/MuiSelect';
+// import { MuiTypography } from './components/MuiTypography';
+
+import MuiCheckbox from './components/MuiCheckbox';
 
 function App() {
   return (
     <div style={{ margin: '2rem' }}>
       {/* <MuiTypography /> */}
       {/* <MuiButton /> */}
-      <MuiSelect />
+      {/* <MuiSelect /> */}
+      <MuiCheckbox />
     </div>
   );
 }
