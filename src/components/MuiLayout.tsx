a9147f76cbf8175e18780d2c9f7da8c2f8031b7c
Vadim Fattakhov <vadfthv@gmail.com>
2024-06-26 19:23:39 -0500

    Paper component

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,34 +1,37 @@
-import { Box, Stack, Divider } from '@mui/material';
+import { Box, Stack, Divider, Paper } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 export default function MuiLayout() {
   return (
-    <Stack
-      sx={{ border: '1px solid' }}
-      direction="row"
-      spacing={2}
-      divider={<Divider orientation="vertical" flexItem />}
-    >
-      <Box
-        sx={{
-          backgroundColor: 'primary.main',
-          color: 'white',
-          height: '100px',
-          width: '100px',
-          padding: '16px',
-          '&:hover': {
-            backgroundColor: 'primary.light',
-          },
-        }}
+    <Paper sx={{ padding: '32px' }} elevation={4}>
+      <Stack
+        sx={{ border: '1px solid' }}
+        direction="row"
+        spacing={2}
+        divider={<Divider orientation="vertical" flexItem />}
       >
-        I am the best and highest paid developer
-      </Box>
-      <Box
-        display="flex"
-        height="100px"
-        width="100px"
-        bgcolor="success.light"
-        p={2}
-      ></Box>
-    </Stack>
+        <Box
+          sx={{
+            backgroundColor: 'primary.main',
+            color: 'white',
+            height: '100px',
+            width: '100px',
+            padding: '16px',
+            '&:hover': {
+              backgroundColor: 'primary.light',
+            },
+          }}
+        >
+          I am the best and highest paid developer
+        </Box>
+        <Box
+          display="flex"
+          height="100px"
+          width="100px"
+          bgcolor="success.light"
+          p={2}
+        ></Box>
+      </Stack>
+    </Paper>
   );
 }
